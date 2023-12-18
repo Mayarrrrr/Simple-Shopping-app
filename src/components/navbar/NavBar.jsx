ad7ce5c4cb21f4f1c2e72a8673461e7d33c26f97
Mayarrrrr <mayar.ateffff@gmail.com>
2023-12-18 20:30:57 +0200

    feat:Cart Icon

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -6,8 +6,12 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { ShopContext } from "../../context/ShoppingContext";
+import { useEffect, useState, useContext } from "react";
 
 const NavBar = () => {
+    const { cartItems } = useContext(ShopContext);
+
     return ( 
         <div> 
             <Navbar bg="dark" data-bs-theme="dark" style={{boxShadow:'0 0 10px 5px #a09fa0'}}>
@@ -20,13 +24,15 @@ const NavBar = () => {
               className="d-inline-block align-top"
               alt="logo"
             />
-          </Navbar.Brand>                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          </Navbar.Brand>                
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
                     <Nav.Link href="/" className="active">Home</Nav.Link>
                     <Nav.Link href="#link">Products</Nav.Link>
                     <Nav.Link href="#link">Offers</Nav.Link>
                     <Nav.Link href="cart"><ShoppingCart size={32}/></Nav.Link>
+                    <Nav.Link href="cart"> {Object.keys(cartItems??{}).length}</Nav.Link>       {/*lw msh la2y 7aga feha rg3lii object fady*/}
                 </Nav>
                 </Navbar.Collapse>
             </Container>
